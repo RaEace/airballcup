2aa47f3cf8604364d5991461e94478fd436c6fa3
Benjamin Lesieux <60974622+BenjaminLesieux@users.noreply.github.com>
2024-10-18 15:20:48 +0200

    fix: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,85 +1,100 @@
-import {FunctionComponent, MouseEventHandler} from "react";
+import {FunctionComponent, HTMLAttributeAnchorTarget} from "react";
 import airballCupLogo from "@/assets/logo.svg";
-import {AlignJustify} from "lucide-react";
+import {Button, buttonVariants} from "@/components/ui/button.tsx";
+import ArrowButton from "@/components/arrow-button.tsx";
+import {AlignJustifyIcon, ArrowRight} from "lucide-react";
 import {
     Sheet,
     SheetClose,
     SheetContent,
     SheetDescription,
-    SheetHeader,
+    SheetFooter,
     SheetTitle,
     SheetTrigger
 } from "@/components/ui/sheet.tsx";
-import {Button, buttonVariants} from "@/components/ui/button.tsx";
-import ArrowButton from "@/components/arrow-button.tsx";
-import {cn} from "@/lib/utils.ts";
+import {cn, handleScrollToSection} from "@/lib/utils.ts";
 
 const Header: FunctionComponent = () => {
-    const links = [["#about", "Notre histoire"], ["#participate", "Comment participer"], ["#cost", "Tarifs et récomponses"], ["#champions", "Gallerie des champions"]];
+    const links: [string, string, HTMLAttributeAnchorTarget | undefined][] = [
+        ["#about", "Notre histoire", undefined],
+        ["#participate", "Comment participer", undefined],
+        ["#cost", "Tarifs et récomponses", undefined],
+        ["#champions", "Gallerie des champions", undefined],
+        ["https://www.instagram.com/airballcup", "Instagram", "_blank"]
+    ];
 
-    const handleScrollToSection: MouseEventHandler<HTMLAnchorElement> = (event) => {
-        event.preventDefault();
-
-        const target = event.currentTarget.getAttribute("href");
-        if (!target) return;
-
-        const targetId = target.substring(1);
-        const targetElement = document.getElementById(targetId);
-        if (!targetElement) return;
-
-        targetElement.scrollIntoView({ behavior: "smooth" });
-    }
-
-    function BurgerMenu() {
+    function InnerBurgerMenu() {
         return <Sheet>
             <SheetTrigger>
-                <button className={"bg-white rounded-full p-2"}>
-                    <AlignJustify color={"#0C0C0F"}/>
-                </button>
+                <Button variant={"outline"} size={"icon"}>
+                    <AlignJustifyIcon/>
+                </Button>
             </SheetTrigger>
-            <SheetContent className={"z-[200] bg-gray-900"}>
-                <SheetHeader>
-                    <SheetTitle className={"font-display uppercase text-2xl text-white"}>
-                        Menu
-                    </SheetTitle>
-                </SheetHeader>
-                <SheetDescription className={"h-full flex flex-col items-center gap-3 mt-10"}>
-                    <ArrowButton className={"ml-4 p-6"} variant={"invertedPrimary"}>
-                        <a href="register">Je m'inscris</a>
-                    </ArrowButton>
-                    { links.map(([href, title]) => (
-                        <SheetClose asChild>
-                            <Button variant={"link"} asChild>
-                                <a href={href} onClick={handleScrollToSection} className={"text-xl"}>
+            <SheetContent side={"full"} className={"z-[101] bg-gray-950"}>
+                <SheetTitle>
+                    <SheetClose>
+                        <Button variant={"link"} asChild>
+                            <a href="/"><img src={airballCupLogo} alt="AirballCup Logo"/></a>
+                        </Button>
+                    </SheetClose>
+                </SheetTitle>
+                <SheetDescription className={"mt-10"}>
+                    <nav className={"flex flex-col items-start"}>
+                        {links.map(([href, title, target]) => (
+                            <SheetClose key={href}>
+                                <a target={target} className={
+                                    cn(buttonVariants({variant: "link"}), "group font-display font-bold text-title-m")
+                                }
+                                   href={href}>
+                                    <ArrowRight
+                                        color={"#E51C21"}
+                                        className={"smooth mr-2 mb-1 sm:size-10 size-4 opacity-0 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 ease-in-out transform translate-x-2"}
+                                    />
                                     {title}
                                 </a>
-                            </Button>
-                        </SheetClose>
-                    ))}
+                            </SheetClose>
+                        ))}
+                    </nav>
                 </SheetDescription>
+                <SheetFooter className={"mt-10"}>
+                    <ArrowButton
+                        size={"sm"}
+                        iconPlacement={"right"}
+                        className={"w-48 mr-4 py-6 px-8 ml-16"}
+                        variant={"primary"}
+                    >
+                        Je m'inscris
+                    </ArrowButton>
+                </SheetFooter>
             </SheetContent>
         </Sheet>
     }
 
     return <header
-        className={"absolute backdrop-blur-sm font-display uppercase z-[100] px-6 flex items-center justify-between w-full h-[104px] bg-transparent"}>
+        className={"absolute backdrop-blur-[2px] font-display uppercase z-[100] px-6 flex items-center justify-between w-full h-[104px] bg-transparent"}>
         <a href="/">
             <img src={airballCupLogo} alt="logo airball cup"/>
         </a>
 
-        <div className={"hidden lg:block"}>
-            { links.map(([href, title]) => (
-                <a href={href} onClick={handleScrollToSection}
-                   className={cn(buttonVariants({variant: "link"}), "text-xl")}>
+        <div className={"hidden lg:flex lg:items-center"}>
+            {links.map(([href, title, target], index) => (
+                <a
+                   key={index}
+                   onClick={target === undefined ? handleScrollToSection : undefined}
+                   className={
+                    cn(buttonVariants({ variant: "link" }), "xl:text-button-m lg:text-[10px]")
+                   }
+                   target={target}
+                   href={href}>
                     {title}
                 </a>
             ))}
-            <ArrowButton className={"ml-4 p-6"} variant={"invertedPrimary"}>
-                Je m'inscris
-            </ArrowButton>
+            <Button size={"sm"} variant={"invertedPrimary"}>
+                Je m'inscris <ArrowRight className={"ml-2 mb-1 size-6"}/>
+            </Button>
         </div>
         <div className={"lg:hidden"}>
-            <BurgerMenu/>
+            <InnerBurgerMenu/>
         </div>
     </header>
 }
