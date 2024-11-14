ec17f5497898106d37ec8e5c8b2eac7a4a6d19d8
Benjamin Lesieux <60974622+BenjaminLesieux@users.noreply.github.com>
2024-11-14 16:16:21 +0100

    feat: add 404, analytics scripts + SEO

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // Outputs a Single-Page Application (SPA).
+    distDir: './dist', // Changes the build output directory to `./dist/`.
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Changes the base path to `/app`.
+    images: { unoptimized: true },
+    webpack: (config) => {
+        config.module.rules.push(
+            {
+                test: /\.md$/,
+                type: 'asset/source',
+            }
+        )
+        return config
+    },
+}
+
+export default nextConfig;
