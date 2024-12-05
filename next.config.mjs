fecebe4b49e243cd830a877ee4f8d7ca41f938f9
Benjamin Lesieux <60974622+BenjaminLesieux@users.noreply.github.com>
2024-12-05 16:49:57 +0100

    feat: change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: './dist', // Changes the build output directory to `./dist/`.
     basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Changes the base path to `/app`.
     images: { unoptimized: true },
     webpack: (config) => {
