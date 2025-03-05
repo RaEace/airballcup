659673c788a00f926bb7d0425aad9bff8df71db5
Benjamin Lesieux <60974622+BenjaminLesieux@users.noreply.github.com>
2025-03-05 22:46:24 +0100

    fix: remove distDir configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import {withPayload} from "@payloadcms/next/withPayload";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: "./dist",
     basePath: '', // Changes the base path to `/app`.
     images: { unoptimized: false },
     webpack: (config) => {
