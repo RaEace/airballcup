50ce1b5ae5532197fe7fdf385a6d6ecc3d929768
Benjamin Lesieux <60974622+BenjaminLesieux@users.noreply.github.com>
2026-05-13 14:52:20 +0200

    chore: remove obsolete i18n and provider files

--- src/intl/en.messages.ts
+++
@@ -1,7 +0,0 @@
-export const englishMessages: Record<string, string> = {
-    errorPageTitle: "Oops!",
-    errorPageMessage: "Sorry, an unexpected error has occurred.",
-
-    // Home.tsx
-    homeTitle: "tournoi mensuel de beer pong a paris",
-  };
